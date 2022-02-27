9887403f831619d34da229457d1482ef6c0233f2
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-02-26 22:49:58 -0600

    add basic test to public-api handler

---
+++ services/public-api/handler.test.ts
@@ -0,0 +1,22 @@
+import { APIGatewayProxyEvent, Context, Callback } from 'aws-lambda';
+import { healthcheck } from './handler';
+
+describe('healtcheck', () => {
+  let subject;
+
+  beforeAll(async () => {
+    subject = await healthcheck(
+      {} as APIGatewayProxyEvent,
+      {} as Context,
+      {} as Callback
+    );
+  });
+
+  it('returns a 200 statusCode', () => {
+    expect(subject.statusCode).toBe(200);
+  });
+
+  it('returns a working message', () => {
+    expect(subject.body).toEqual('public-api is working!');
+  });
+});
