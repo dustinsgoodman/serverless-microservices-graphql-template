bb59d39b42ea8c9139fbdc4e83c1f1734ce54329
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-03-01 18:09:38 -0600

    add console to project.json for libs

--- tools/generators/lib/index.ts
+++ tools/generators/lib/index.ts
@@ -5,10 +5,16 @@ import {
   readProjectConfiguration,
   generateFiles,
   joinPathFragments,
+  getWorkspaceLayout,
+  updateProjectConfiguration,
 } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
-export default async function (tree: Tree, schema: any) {
+type Schema = {
+  readonly name: string;
+};
+
+export default async function (tree: Tree, schema: Schema) {
   await libraryGenerator(tree, { name: schema.name });
   const libraryRoot = readProjectConfiguration(tree, schema.name).root;
 
@@ -19,8 +25,28 @@ export default async function (tree: Tree, schema: any) {
     schema
   );
 
+  updateProject(tree, schema);
+
   await formatFiles(tree);
   return () => {
     installPackagesTask(tree);
   };
+}
+
+// See https://github.com/nrwl/nx/blob/efedd2eff78700a72bcc30bdf7450656860a4ffb/packages/node/src/generators/library/library.ts#L130-L156
+function updateProject(tree: Tree, options: Schema) {
+  const project = readProjectConfiguration(tree, options.name);
+
+  project.targets = project.targets || {};
+  project.targets.console = {
+    executor: './tools/executors/workspace:run-command',
+    options: {
+      cwd: 'libs/shared',
+      color: true,
+      command:
+        'node --experimental-repl-await -r ts-node/register ./console.js',
+    },
+  };
+
+  updateProjectConfiguration(tree, options.name, project);
 }
