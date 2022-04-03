a8646e54590a3265beba4ddf61a9c38b982ff2a5
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-04-03 00:56:20 -0500

    feat: scaffold out new example-service

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
     '<rootDir>/services/public-api',
     '<rootDir>/services/background-jobs',
     '<rootDir>/libs/shared',
+    '<rootDir>/services/example-service',
   ],
 };
---
+++ services/example-service/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  displayName: 'example-service',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  coverageDirectory: '../../coverage/services/example-service',
+};
