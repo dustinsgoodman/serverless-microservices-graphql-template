25bb0c4e4ef1a5db76ab9279b791b707ad7f2f5d
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-04-03 01:12:53 -0500

    refactor: update background-jobs to have only SQSHandlers

---
+++ services/background-jobs/handlers/demo.test.ts
@@ -0,0 +1,38 @@
+import { Context, Callback, SQSEvent } from 'aws-lambda';
+import { handler } from './demo';
+
+describe('demo', () => {
+  let subject;
+  let logMock;
+
+  beforeAll(async () => {
+    logMock = jest.spyOn(console, 'log').mockImplementation(() => ({}));
+    subject = await handler(
+      {
+        Records: [
+          {
+            body: 'Hello',
+          },
+          {
+            body: 'World',
+          },
+        ],
+      } as SQSEvent,
+      {} as Context,
+      {} as Callback
+    );
+  });
+
+  afterAll(() => {
+    logMock.mockReset();
+  });
+
+  it('processes both messages', () => {
+    expect(console.log).toHaveBeenCalledWith('"Hello"');
+    expect(console.log).toHaveBeenCalledWith('"World"');
+  });
+
+  it('returns nothing', () => {
+    expect(subject).toBeUndefined();
+  });
+});
