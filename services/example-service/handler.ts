df8bedbd6ee5be1f62d24394abd1788f827abab5
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-04-03 01:13:27 -0500

    feat: add example job generator to example-service

--- services/example-service/handler.ts
+++ services/example-service/handler.ts
@@ -1,8 +1,32 @@
+import { SQS } from 'aws-sdk';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 
 export const healthcheck: APIGatewayProxyHandler = async (_event, _context) => {
   return {
     statusCode: 200,
     body: 'example-service is working!',
+  };
+};
+
+export const generateDemoJobs: APIGatewayProxyHandler = async () => {
+  const sqs = new SQS({ apiVersion: '2012-11-05' });
+  const resp = await sqs
+    .sendMessage({
+      QueueUrl: 'http://localhost:9324/000000000000/DemoQueue',
+      MessageBody: 'Hello World!',
+    })
+    .promise();
+
+  if (resp.MessageId) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(resp),
+    };
+  }
+
+  console.error(resp);
+  return {
+    statusCode: 400,
+    body: 'Could not send message',
   };
 };
