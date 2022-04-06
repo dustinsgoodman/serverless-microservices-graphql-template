ee56ce1c8fbef813f42ef026a8554268ef19adc5
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-04-06 09:27:52 -0500

    refactor: convert background-jobs config to typescript

---
+++ services/background-jobs/serverless.ts
@@ -0,0 +1,53 @@
+import type { Serverless } from 'serverless/aws';
+import { custom } from '../../serverless.common';
+
+const serverlessConfiguration: Serverless = {
+  service: 'background-jobs',
+  frameworkVersion: '3',
+  plugins: [
+    'serverless-s3-remover',
+    'serverless-esbuild',
+    'serverless-analyze-bundle-plugin',
+    'serverless-offline-sqs',
+    'serverless-offline',
+  ],
+  useDotenv: true,
+  custom,
+  package: {
+    individually: true,
+    patterns: ['handler.js', '!node_modules/**'],
+    excludeDevDependencies: true,
+  },
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    stage: "${opt:stage, 'dev'}",
+    region: "${opt:region, 'us-east-1'}",
+  },
+  functions: {
+    demo: {
+      handler: 'handlers/demo.handler',
+      events: [
+        {
+          sqs: {
+            arn: {
+              'Fn::GetAtt': ['DemoQueue', 'Arn'],
+            },
+          },
+        },
+      ],
+    },
+  },
+  resources: {
+    Resources: {
+      DemoQueue: {
+        Type: 'AWS::SQS::Queue',
+        Properties: {
+          QueueName: 'DemoQueue',
+        },
+      },
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
