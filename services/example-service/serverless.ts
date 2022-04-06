956a40e173e3664ca80e635ecf045becfa2a6517
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-04-05 21:28:49 -0500

    refactor: convert example-service config to typescript

---
+++ services/example-service/serverless.ts
@@ -0,0 +1,44 @@
+import type { Serverless } from 'serverless/aws';
+import { custom } from '../../serverless.common';
+
+const serverlessConfiguration: Serverless = {
+  service: 'example-service',
+  frameworkVersion: '3',
+  plugins: [
+    'serverless-s3-remover',
+    'serverless-esbuild',
+    'serverless-analyze-bundle-plugin',
+    'serverless-offline',
+  ],
+  useDotenv: true,
+  custom,
+  package: {
+    individually: true,
+    patterns: ['handler.js', '!node_modules/**'],
+    excludeDevDependencies: true,
+  },
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    stage: "${opt:stage, 'dev'}",
+    region: "${opt:region, 'us-east-1'}",
+    environment: {
+      DEMO_QUEUE_URL: '${env:DEMO_QUEUE_URL}',
+    },
+  },
+  functions: {
+    generateDemoJobs: {
+      handler: 'handlers/generateDemoJob.handler',
+      events: [
+        {
+          httpApi: {
+            method: 'get',
+            path: '/generateDemoJob',
+          },
+        },
+      ],
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
