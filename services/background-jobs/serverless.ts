998c8ca510c4a083afec4f8bee4aec3db96ea618
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-07-16 10:23:20 -0500

    chore!: upgrade template to use Node 16
    BREAKING CHANGE: Forces the template to Node 16 where it was previously Node 14. Support for Node 14 has not been dropped officially so this commit may be reverted to utilize Node 14 over 16.

--- services/background-jobs/serverless.ts
+++ services/background-jobs/serverless.ts
@@ -21,7 +21,7 @@ const serverlessConfiguration: Serverless = {
   },
   provider: {
     name: 'aws',
-    runtime: 'nodejs14.x',
+    runtime: 'nodejs16.x',
     stage: "${opt:stage, 'dev'}",
     region: "${opt:region, 'us-east-1'}",
     memorySize: 512, // default: 1024MB
